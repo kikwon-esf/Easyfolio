76c8f6715fd96c95b181e9b4f83fa07a0c0a482a
bigbabykim <el2013@naver.com>
2024-02-25 17:10:49 +0900

    0225 1710

--- src/main/resources/static/js/content/mainpage.js
+++ src/main/resources/static/js/content/mainpage.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function() {
     $('.m1_slide').slick({
         autoplay: true,
         autoplaySpeed: 5000,
         speed: 1000,
-        vertical: false
+        slidesToShow: 1,
     });
 });
