10fb8917450bf0813896e682c9631fa8aa501fa9
bigbabykim <el2013@naver.com>
2024-02-27 16:36:55 +0900

    0227 1636

--- src/main/resources/static/js/content/mainpage.js
+++ src/main/resources/static/js/content/mainpage.js
@@ -1,17 +1,4 @@
 // 메인1
-document.addEventListener("DOMContentLoaded", function() {
-    $('.m1_slide').slick({
-        autoplay: true,
-        autoplaySpeed: 3000,
-        speed: 1000,
-        slidesToShow: 1,
-        variableWidth: true,
-        dots: true,
-        prevArrow: '.btn.prev',
-        nextArrow: '.btn.next',
-        appendDots: '.btn.pager',
-    });
-});
 document.addEventListener("mouseover", function (event) {
     var buttons = document.querySelectorAll('.btn');
 
@@ -36,6 +23,49 @@ $('.btn.play').click(function () {
     $(this).hide();
     $('.btn.pause').show();
 });
+$(document).ready(function(){
+    var m1Slide = $('.m1_slide');
 
-// 메인2
+    m1Slide.slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        slidesToShow: 1,
+        variableWidth: true,
+        dots: true,
+        prevArrow: '.btn.prev',
+        nextArrow: '.btn.next',
+        appendDots: '.btn.pager',
+    });
 
+    var totalSlides = m1Slide.slick('getSlick').slideCount;
+
+    m1Slide.on('afterChange', function(event, slick, currentSlide){
+        if(currentSlide < 10){
+            $('.current-slide').text("0" + (currentSlide + 1));
+        } else{
+            $('.current-slide').text(currentSlide + 1); 
+        }
+        if(totalSlides < 10){
+            $('.total-slides').text("0" + (totalSlides));
+        } else{
+            $('.total-slides').text(totalSlides);
+        }
+    });
+});
+// 메인1 popup
+// document.addEventListener("mouseover", function (event) {
+//     var seSlides = document.querySelectorAll('.sa_slides');
+//     var targetElement = event.target;
+//     // 'sa_slides' 클래스를 가진 요소인지 확인
+//     if (targetElement.classList.contains('sa_slides')) {
+//         // 해당 요소의 opacity를 1로 변경
+//         targetElement.style.opacity = 1;
+//     }
+// });
+$('.btn.menu').click(function () {
+    $('.slideAll').slideDown();
+});
+$('.btnClose').click(function () {
+    $('.slideAll').slideUp();
+});
