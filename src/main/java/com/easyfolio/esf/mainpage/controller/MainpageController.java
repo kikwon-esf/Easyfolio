7102f4a039e79d3a7dc17b769f22c171f1039a42
bigbabykim <el2013@naver.com>
2024-03-13 17:10:07 +0900

    0313 1710

--- src/main/java/com/easyfolio/esf/mainpage/controller/MainpageController.java
+++ src/main/java/com/easyfolio/esf/mainpage/controller/MainpageController.java
@@ -1,7 +1,9 @@
 package com.easyfolio.esf.mainpage.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mainpage")
 @RequiredArgsConstructor
 public class MainpageController {
+
     @GetMapping("/main")
-    public String mainpage(){
+    public String homePage(Model model, Authentication authentication) {
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated();
+        model.addAttribute("isAuthenticated", isAuthenticated);
         return "content/indexpage/mainpage";
     }
-
     @GetMapping("/test")
     public String testpage(){
         return "content/indexpage/testpage";
--- src/main/java/com/easyfolio/esf/search/service/SearchServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package com.easyfolio.esf.search.service;
-
-public class SearchServiceImpl {
-}
