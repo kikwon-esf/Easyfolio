c9ce17ff4cbc1d311a2ab3c6dd32cc08a80e021a
PeterJiseong <wltjd9407@naver.com>
2024-03-14 15:51:46 +0900

    임시 - 인터셉터 구현(로그인 확인)

---
+++ src/main/java/com/easyfolio/esf/config/interceptor/WebMvcConfiguration.java
@@ -0,0 +1,30 @@
+package com.easyfolio.esf.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    private final CheckLoginInterceptor checkLoginInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //예외 등록(이 요청에는 인터셉터 동작 안함, 불필요한 동작 막음)
+        List<String> exceptionPath = new ArrayList<>();
+        exceptionPath.add("/css/**");
+        exceptionPath.add("/js/**");
+        exceptionPath.add("/img/**");
+
+
+        //인터셉터 동작
+        registry.addInterceptor(checkLoginInterceptor) // 해당 인터셉터 적용
+                .excludePathPatterns(exceptionPath) //list로 등록한 예외 적용
+                .addPathPatterns("/**"); //그 외 나머지 동작
+    }
+}
