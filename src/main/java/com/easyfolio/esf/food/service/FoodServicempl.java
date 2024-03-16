be6f3e233f6f9fa44b7f8c3c278853789a58a6e5
bigbabykim <el2013@naver.com>
2024-03-16 17:19:36 +0900

    0316 1719

--- src/main/java/com/easyfolio/esf/food/service/FoodServicempl.java
+++ src/main/java/com/easyfolio/esf/food/service/FoodServicempl.java
@@ -12,73 +12,12 @@ import java.util.List;
 public class FoodServicempl implements FoodService{
     private final SqlSessionTemplate sqlSession;
 
+    @Override
+    public List<FoodVO> allFoodList() {
+        return sqlSession.selectList("foodMapper.allFoodList");
+    }
+
     // 음식 전체 목록 조회
-    public List<FoodVO> foodList() {
-        return sqlSession.selectList("foodMapper.foodList");
-    }
-    // 음식 상세 조회
-    public FoodVO foodDetail(FoodVO foodVO) {
-        return sqlSession.selectOne("foodMapper.foodDetail", foodVO);
-    }
 
-    // 음식명 조회
-    public List<FoodVO> foodNameList(FoodVO foodVO) {
-        return sqlSession.selectList("foodMapper.foodNameList", foodVO);
-    }
-
-    // 음식 타입 조회
-    public List<FoodVO> foodTypeList(FoodVO foodVO) {
-        return sqlSession.selectList("foodMapper.foodTypeList", foodVO);
-    }
-
-    @Override
-    public List<FoodVO> foodMTRLList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodKindList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodServeList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodDifficultList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodTimeList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodREGDTList(FoodVO foodVO) {
-        return null;
-    }
-
-    @Override
-    public List<FoodVO> foodUsageList(FoodVO foodVO) {
-        return null;
-    }
-
-    // 음식 등록
-    public int insertFood(FoodVO foodVO) {
-        return sqlSession.insert("foodMapper.insertFood", foodVO);
-    }
-
-    // 음식 수정
-    public int updateFood(FoodVO foodVO) {
-        return sqlSession.update("foodMapper.updateFood", foodVO);
-    }
-
-    // 음식 삭제
-    public int deleteFood(FoodVO foodVO) {
-        return sqlSession.delete("foodMapper.deleteFood", foodVO);
-    }
 
 }
--- src/main/resources/static/js/fragment/main_header.js
+++ src/main/resources/static/js/fragment/main_header.js
@@ -67,7 +67,7 @@ document.addEventListener("mouseover", function (event) {
 })
 
 //로그아웃
-const logout = document.getElementById("logout");
+var logout = document.getElementById("logout");
 logout.addEventListener("click",(e)=>{
     let active = confirm("로그아웃을 하시겠습니까?");
     if(!active){
