47a49aad4f9709943c33e571f13db4834c596ce0
SJY5261 <sjy5261@gmail.com>
2024-04-11 22:03:54 +0900

    0411 디비 수정

--- src/main/resources/static/js/content/csc/qna/csc_qnaList.js
+++ src/main/resources/static/js/content/csc/qna/csc_qnaList.js
@@ -1,29 +1,46 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
+    $(document).ready(function () {
+        const aQnaCode = $('.aQnaCode').val();
+        const bQnaCodes = $('.bQnaCode');
+        bQnaCodes.each(function () {
+            if ($(this).val() === aQnaCode) {
+                const qnaInner = $(this).closest('.qnaInner');
+                qnaInner.addClass('on');
+                
+                // 제이쿼리로 slideToggle() 적용
+                qnaInner.parent().find('.ansBox').slideToggle();
+                
+                qnaInner.parent().find('.qnaBox').addClass('on');
+                qnaInner.parent().find('.qnaArrow').addClass('rotate');
+            }
+        });
+    });
+
     var qnaBlocks = document.querySelectorAll('.qnaBlock');
     var qnaBlocksArray = Array.from(qnaBlocks);
 
     qnaBlocksArray.forEach(function (qnaBlock) {
         var dnuBtnBox = qnaBlock.querySelector('.dnuBtnBox');
         var qnaInner = qnaBlock.querySelector('.qnaInner');
-        var ansBox = qnaBlock.querySelector('.ansBox'); 
+        var ansBox = qnaBlock.querySelector('.ansBox');
 
         qnaInner.addEventListener('click', function () {
             $(dnuBtnBox).toggleClass('on');
-            $(ansBox).slideToggle(); 
+            $(ansBox).slideToggle();
             $(qnaInner).find('.qnaBox').toggleClass('on');
             $(qnaInner).find('.qnaArrow').toggleClass('rotate');
         });
 
         ansBox.addEventListener('click', function (event) {
-            event.stopPropagation(); 
+            event.stopPropagation();
         });
     });
 });
 var annSearch = document.querySelector('.annSearch');
 var annSearchBlock = document.querySelector('.annSearchBlock');
-if(annSearch != null){
+if (annSearch != null) {
     annSearch.addEventListener("click", function () {
         annSearchBlock.style.border = "2px solid #F29221";
         annSearchBlock.style.backgroundColor = "transparent"
@@ -36,8 +53,10 @@ if(annSearch != null){
             annSearch.style.color = "#999";
             annSearch.placeholder = '자주 찾는 질문 검색';
         }
-    
+
     });
 }
+
+
 
 
