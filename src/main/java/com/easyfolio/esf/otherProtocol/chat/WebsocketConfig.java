c78398a6e415ecd8dbea7a8a34ffed6e8686ad39
PeterJiseong <wltjd9407@naver.com>
2024-06-04 23:45:01 +0900

    채팅방 백 기능 구현중 - 주석

--- src/main/java/com/easyfolio/esf/otherProtocol/chat/WebsocketConfig.java
+++ src/main/java/com/easyfolio/esf/otherProtocol/chat/WebsocketConfig.java
@@ -1,13 +1,13 @@
-package com.easyfolio.esf.otherProtocol.chat;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
-
-@Configuration
-@RequiredArgsConstructor
-@EnableWebSocketMessageBroker
-public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
-
-
-}
+//package com.easyfolio.esf.otherProtocol.chat;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.*;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//@EnableWebSocketMessageBroker
+//public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//
+//}
