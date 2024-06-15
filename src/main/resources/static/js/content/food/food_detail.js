f4e1306097b73b24dddd97146a64ebdaaa7c3cb2
PeterJiseong <wltjd9407@naver.com>
2024-06-16 06:28:53 +0900

    mypage_comment

--- src/main/resources/static/js/content/food/food_detail.js
+++ src/main/resources/static/js/content/food/food_detail.js
@@ -1,4 +1,3 @@
-const response_commentId = document.querySelector("#response_commentId")?.value;
 window.addEventListener('DOMContentLoaded', function () {
     var mtrlMtElements = document.querySelectorAll('.recipe_mtrl .mtrlMt');
 
@@ -8,21 +7,9 @@ window.addEventListener('DOMContentLoaded', function () {
             element.classList.add('full');
         }
     });
-    console.dir(response_commentId == "COMM_00008")
-    if(response_commentId != null || undefined){
-        const commentNode = this.document.getElementById(String(response_commentId));
-        console.dir(commentNode);
-
-        scrollTo(findScrollValue(toString(commentNode)));  
-    }
+    
     
     
 });
 
 
-function findScrollValue(commentNode){
-
-    let rect = commentNode.getBoundingClientRect();
-    console.log(rect.y)
-    return rect.y+500;
-}
