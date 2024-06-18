a78e75047efb34771721dd346b255045df4410da
bigbabykim <el2013@naver.com>
2024-06-19 00:29:02 +0900

    0619 0028

--- src/main/resources/static/js/content/weather/weather.js
+++ src/main/resources/static/js/content/weather/weather.js
@@ -1,126 +1,97 @@
+// TMP = 기온
+// SKY = 하늘 상태	코드값	맑음(1), 구름많음(3), 흐림(4)
+// PTY = 강수형태	코드값	없음(0), 비(1), 비/눈(2), 눈(3), 소나기(4)
+// POP = 강수확률 	%
+// PCP 1시간 강수량	범주 1mm
 $(document).ready(function() {
-    var arr = [];
-    var today = new Date();
-    var week = new Array('일', '월', '화', '수', '목', '금', '토');
-    var year = today.getFullYear();
-    var month = today.getMonth() + 1;
-    var day = today.getDate();
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var hours_al = new Array('02', '05', '08', '11', '14', '17', '20', '23');
-    var korea = [
-        {'region': '서울', 'nx': 60, 'ny': 127},
-        {'region': '인천', 'nx': 55, 'ny': 124},
-        {'region': '경기도', 'nx': 60, 'ny': 121},
-        {'region': '강원도', 'nx': 92, 'ny': 131},
-        {'region': '충청북도', 'nx': 69, 'ny': 106},
-        {'region': '충청남도', 'nx': 68, 'ny': 100},
-        {'region': '전라북도', 'nx': 63, 'ny': 89},
-        {'region': '전라남도', 'nx': 50, 'ny': 67},
-        {'region': '경상남도', 'nx': 90, 'ny': 77},
-        {'region': '경상북도', 'nx': 91, 'ny': 106},
-        {'region': '제주도', 'nx': 52, 'ny': 38}
-    ];
+    var apiKey = "3Rb5ATbVd0%2BgY821Hz8%2F%2F7ZsRJUK0o%2FtJsjpaoMRh%2F8MSD3%2B1HPjEzTtMyYOhq37TtKEdFpTMZEOJDsDAnyx2A%3D%3D";
+    var baseDate = "20240618";
+    var baseTime = "2000";
+    var nx = 55;
+    var ny = 127;
+    var apiUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
+    var dataType = "JSON";
 
-    for (var i = 0; i < hours_al.length; i++) {
-        var h = hours_al[i] - hours;
-        if (h == -1 || h == 0 || h == -2) {
-            var now = hours_al[i];
-        }
-        if (hours == 0) {
-            var now = hours_al[7];
-        }
-    }
+    // API 요청 URL 생성
+    var url = `${apiUrl}?ServiceKey=${apiKey}&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}&dataType=${dataType}`;
 
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (month < 10) {
-        month = '0' + month;
-    }
-    if (day < 10) {
-        day = '0' + day;
-    }
-
-    today = year + "" + month + "" + day;
-
-    $.each(korea, function(j, k) {
-        var _nx = korea[j].nx,
-            _ny = korea[j].ny,
-            region = korea[j].region,
-            apikey = "3Rb5ATbVd0%2BgY821Hz8%2F%2F7ZsRJUK0o%2FtJsjpaoMRh%2F8MSD3%2B1HPjEzTtMyYOhq37TtKEdFpTMZEOJDsDAnyx2A%3D%3D",
-            ForecastGribURL = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData";
-        ForecastGribURL += "?ServiceKey=" + apikey;
-        ForecastGribURL += "&base_date=" + today;
-        ForecastGribURL += "&base_time=" + now + "00";
-        ForecastGribURL += "&nx=" + _nx + "&ny=" + _ny;
-        ForecastGribURL += "&dataType=JSON"; // JSON 형식으로 요청
-
-        arr.push({'url': ForecastGribURL, 'region': region});
-
-        $.ajax({
-            url: arr[j].url,
-            type: 'GET',
-            dataType: 'json', // JSON 형식으로 응답 받기
-            success: function(data) {
-                var items = data.response.body.items.item;
-                var temp = '';
-                var sky = '';
-                var rain = '';
-
-                $.each(items, function(i, item) {
-                    if (item.category == 'T3H') {
-                        temp = item.fcstValue; // 3시간 온도
-                    }
-                    if (item.category == 'SKY') {
-                        sky = item.fcstValue; // 하늘상태
-                    }
-                    if (item.category == 'PTY') {
-                        rain = item.fcstValue; // 강수형태
-                    }
-                });
-
-                $('.weather').append('<li class="list-group-item weather_li' + j + '"></li>');
-                $('.weather_li' + j).addClass('in' + j);
-                $('.in' + j).html(temp + " ℃"); // 온도
-                $('.in' + j).prepend(arr[j].region + '&emsp;'); // 지역이름
-
-                if (rain != 0) {
-                    switch (rain) {
-                        case '1':
-                            $('.in' + j).append(" / 비");
-                            $('.in' + j).prepend('<i class="fas fa-cloud-showers-heavy"></i>&emsp;');
-                            break;
-                        case '2':
-                            $('.in' + j).append(" / 비/눈");
-                            $('.in' + j).prepend('<i class="fas fa-cloud-rain"></i>&emsp;');
-                            break;
-                        case '3':
-                            $('.in' + j).append(" / 눈");
-                            $('.in' + j).prepend('<i class="fas fa-snowflake"></i>&emsp;');
-                            break;
-                    }
-                } else {
-                    switch (sky) {
-                        case '1':
-                            $('.in' + j).append(" / 맑음");
-                            $('.in' + j).prepend('<i class="fas fa-sun"></i>&emsp;');
-                            break;
-                        case '2':
-                            $('.in' + j).append(" / 구름조금");
-                            $('.in' + j).prepend('<i class="fas fa-cloud-sun"></i>&emsp;');
-                            break;
-                        case '3':
-                            $('.in' + j).append(" / 구름많음");
-                            $('.in' + j).prepend('<i class="fas fa-cloud"></i>&emsp;');
-                            break;
-                        case '4':
-                            $('.in' + j).append(" / 흐림");
-                            $('.in' + j).prepend('<i class="fas fa-smog"></i>&emsp;');
-                            break;
-                    }
+    // AJAX를 통한 비동기 요청
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            var temperature = null;
+            var skyCondition = null;
+            var precipitationType = null;
+            var precipitationProbability = null;
+        
+            var items = data.response.body.items.item;
+            for (var i = 0; i < items.length; i++) {
+                var category = items[i].category;
+                var fcstValue = items[i].fcstValue;
+        
+                switch (category) {
+                    case "TMP":
+                        temperature = fcstValue + " ℃";
+                        break;
+                    case "SKY":
+                        switch (fcstValue) {
+                            case "1":
+                                skyCondition = "맑음";
+                                
+                                break;
+                            case "3":
+                                skyCondition = "구름많음";
+                                break;
+                            case "4":
+                                skyCondition = "흐림";
+                                break;
+                            default:
+                                skyCondition = "알 수 없음";
+                                break;
+                        }
+                        break;
+                    case "PTY":
+                        switch (fcstValue) {
+                            case "0":
+                                precipitationType = "없음";
+                                break;
+                            case "1":
+                                precipitationType = "비";
+                                break;
+                            case "2":
+                                precipitationType = "비/눈";
+                                break;
+                            case "3":
+                                precipitationType = "눈";
+                                break;
+                            case "4":
+                                precipitationType = "소나기";
+                                break;
+                            default:
+                                precipitationType = "알 수 없음";
+                                break;
+                        }
+                        break;
+                    case "POP":
+                        precipitationProbability = fcstValue + " %";
+                        break;
+                    default:
+                        // 다른 카테고리의 경우 아무 작업도 하지 않음
+                        break;
                 }
             }
-        });
+        
+            // 추출한 값을 출력해보기
+            console.log("기온:", temperature);
+            console.log("하늘 상태:", skyCondition);
+            console.log("강수 형태:", precipitationType);
+            console.log("강수 확률:", precipitationProbability);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // 요청 실패 시 에러 처리
+            console.error("API 요청 실패:", textStatus, errorThrown);
+        }
     });
 });
