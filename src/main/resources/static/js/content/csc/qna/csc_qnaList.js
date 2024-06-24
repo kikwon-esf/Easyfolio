de6b7efdd46d61aded52ebc395356f6dd6226ae4
SJY5261 <sjy5261@gmail.com>
2024-06-24 15:35:10 +0900

    0624 탭완료

--- src/main/resources/static/js/content/csc/qna/csc_qnaList.js
+++ src/main/resources/static/js/content/csc/qna/csc_qnaList.js
@@ -1,5 +1,26 @@
 // 자주 찾는 질문을 클릭해서 넘어왔을 때 해당 질문 열리는 코드
 document.addEventListener("DOMContentLoaded", function () {
+    var nowPage = document.querySelector('#nowPage').value;
+
+    var pageButtons = document.querySelectorAll('.page_numBtn');
+
+    pageButtons.forEach(function(button) {
+        if (button.textContent === nowPage) {
+            button.classList.add('active'); // 여기서 'active'는 원하는 클래스명입니다.
+        }
+    });
+
+    var currentPageUrl = window.location.href;
+    if (currentPageUrl.endsWith("/food/searchFoodPage")) {
+        var numBtnSpans = document.querySelectorAll(".page_numBtn span");
+        numBtnSpans.forEach(function(span) {
+            if (span.textContent.trim() === '1') {
+                span.closest('.page_numBtn').classList.add('active');
+            }
+        });
+        
+    }
+
     setTimeout(function() {
         const aQnaCode = $('.aQnaCode').val();
         const bQnaCodes = $('.bQnaCode');
@@ -10,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }, 100); 
 });
+
 // 서머노트
 $(document).ready(function () {
     $('.editor').summernote({
