1e7d2cd3754ecfecb43936ca95b570de083f290d
PeterJiseong <wltjd9407@naver.com>
2024-06-27 01:39:12 +0900

    wideSearchgit

--- src/main/resources/static/js/content/csc/inq/csc_insertInq.js
+++ src/main/resources/static/js/content/csc/inq/csc_insertInq.js
@@ -19,3 +19,40 @@ function readURL(input) {
     };
 
 }
+
+function loginGo() {
+    let pw = document.querySelector('#memberPw');
+    let pwCheck = document.querySelector('#memberPwch');
+    let name = document.querySelector('#memberName');
+    let tel = document.querySelector('#memberTel');
+
+    if (inputMemId.value.trim() == "") {
+        idBox.classList.toggle('fail');
+        inputMemId.placeholder = "아이디를 입력해주세요";
+    } else if(!isIdChecked){
+        idBox.classList.toggle('fail');
+        inputMemId.value='';
+        inputMemId.placeholder = "아이디 중복을 확인 해 주세요";
+    } 
+    else if (pw.value === '') {
+        pw.closest('.inputBox').classList.toggle('fail');
+        pw.placeholder = "비밀번호를 입력해 주세요.";
+    } else if (pwCheck.value === '') {
+        pwCheck.closest('.inputBox').classList.toggle('fail');
+        pwCheck.placeholder = "비밀번호를 확인 해 주세요.";
+    } else if (pw.value !== pwCheck.value) {
+        pwCheck.closest('.inputBox').classList.toggle('fail');
+        pwCheck.type = "text";
+        pwCheck.value='';
+        pwCheck.placeholder = "비밀번호가 일치하지 않습니다.";
+        // 클릭했을때 비번처럼 되게 다시 처리해야함
+    } else if (name.value.trim() === "") {
+        name.closest('.inputBox').classList.toggle('fail');
+        name.placeholder = "이름을 입력해 주세요";
+    } else if (tel.value.trim() === "") {
+        tel.closest('.inputBox').classList.toggle('fail');
+        tel.placeholder = "전화번호를 입력해 주세요";
+    } else {
+        document.querySelector('#joinForm').submit();
+    }
+}
